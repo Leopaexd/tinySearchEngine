1c8b892db4f8ddae290957f30484d2cee709d29a
Oliver <oliver.glant@gmail.com>
2017-10-01 11:02:32 +0200

    Indexing and simple search working with great performance!

--- src/main/java/tinySearchEngine/TinySearchEngine.java
+++ src/main/java/tinySearchEngine/TinySearchEngine.java
@@ -12,41 +12,33 @@ import se.kth.id1020.util.Word;
 public class TinySearchEngine implements TinySearchEngineBase{
 	ArrayList<entry> index = new ArrayList<entry>();
 	
-	public TinySearchEngine() { 
-		//Constructor that creates an empty index where entries can be inserted
-	}
-	
 	public void insert (Word word, Attributes attr) {
-		//Insertion
-		//System.out.println("Index size: " + index.size());
-		
+		//Create an entry for every new word and place it at the correct place in the index by using binary search.
+		//If word is already in index, add new attribute to word.
+		if (word.word.compareToIgnoreCase("A") < 0) return; //Prevents numbers from being placed as words in the index
 		entry newEntry = new entry(word, attr);
 		int point = Collections.binarySearch(index, newEntry);
-		if (point >= 0) {
-			index.get(point).addAttribute(attr);
-		} else {
-			index.add(-(point+1), newEntry);
-		}
-		
-		if (index.size() == 5000) for (entry entry: index) System.out.println(entry.word.word);
-		
+		if (point >= 0) index.get(point).addAttribute(attr);
+		else index.add(-(point+1), newEntry);
 	}
 	
 	//Search
 	public List<Document> search (String query) {
-		//Find word matching query in the linked list using binary search
+		//Find word matching query in the index using binary search and add all documents where the word
+		//is found to list 'results' and return the list
 		List<Document> results = new ArrayList<Document>();
-		for (int i = 0; i < 25; i++) System.out.println(index.get(i).word.word);
 		int high = index.size()-1;
-		int low = 0;
-		
+		int low = 0;	
 		while (high >= low) {
 			int i = low + (high - low)/2;
-			int comparison = index.get(i).word.word.compareTo(query);
-			if (comparison > 0) low = i+1; //word is in the right half
-			else if (comparison < 0) high = i-1; //word is in the left half
+			int comparison = index.get(i).word.word.compareToIgnoreCase(query);
+			if (comparison < 0) low = i+1; //word is in the right half
+			else if (comparison > 0) high = i-1; //word is in the left half 
 			else if (comparison == 0) {
-				for (Attributes attribute: index.get(i).attributeList) results.add(attribute.document);
+				for (Attributes attribute : index.get(i).attributeList) {
+					if (results.contains(attribute.document) == false) results.add(attribute.document); 
+				} //prevent the same document from being listed multiple times
+				break;
 			} 
 		}
 		return results;
